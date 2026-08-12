ff1a21cea95c598a2436e4bd062dbf94c6b02719
dima <kobylynskyjd@gmail.com>
2026-08-12 14:14:54 +0300

    Finish modal

--- primal_training/static/primal_training/Js/modal.js
+++ primal_training/static/primal_training/Js/modal.js
@@ -7,6 +7,11 @@ document.addEventListener('DOMContentLoaded',() =>  {
 
     const modalForm = document.querySelector('.modal-form')
     
+    const closeModal = () => {
+        modal.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+    };
+
 
     openBtns.forEach( btn =>{
         btn.addEventListener("click" ,() => {
@@ -17,17 +22,115 @@ document.addEventListener('DOMContentLoaded',() =>  {
             modalTite.innerHTML = `BOOK: <span>${classTitle}</span>`
 
             modal.classList.add("active")
+            document.body.classList.add("no-scroll");
         })
     })
 
-    closeBtn.addEventListener('click', () => {
-        modal.classList.remove('active')
-    })
+
+    closeBtn.addEventListener('click', closeModal);
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    
 
 
-    modalForm.addEventListener('submit', (e) => {
-        e.preventDefault()
+if (modalForm) {
+    const requiredInputs = modalForm.querySelectorAll('input[required]');
+        requiredInputs.forEach (input  => {
+            const parentField = input.parentElement;
+            const label = parentField.querySelector('label')
+            let errorMsg = parentField.querySelector('.error-text');
 
-        console.log("Form send!");
-    })
-})
+            input.addEventListener('input', () => {
+                if(input.value.trim() !== '') {
+                    input.style.borderColor = '';
+                    input.style.backgroundColor = '';
+
+                    const parentField = input.parentElement;
+                    const errorMsg = parentField.querySelector('.error-text');
+                    
+                    if(label) {
+                        label.style.color = ''
+                    }
+
+                    if(errorMsg) {
+                        errorMsg.remove()
+                    }
+                }
+            })
+        })
+
+
+        modalForm.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const formData = new FormData(modalForm);
+            let isValid = true
+            
+            requiredInputs.forEach(input => {
+                const parentField = input.parentElement;
+                const label = parentField.querySelector('label');
+                let errorMsg = parentField.querySelector('.error-text');
+                if(input.value.trim() === '') {
+                    isValid = false
+                    input.style.borderColor = "#ff3333"
+                    input.style.backgroundColor = "#fff0f0"
+                    if(label) {
+                        label.style.color = '#ff3333'
+                    }
+
+                    if(!errorMsg) {
+                        errorMsg = document.createElement('span');
+                        errorMsg.className = 'error-text';
+                        errorMsg.style.color = '#ff3333';
+                        errorMsg.style.fontSize = '11px';
+                        errorMsg.style.fontWeight = '700';
+                        errorMsg.style.marginTop = '4px';
+                        parentField.appendChild(errorMsg);
+                        const labelText = label ? label.textContent.trim() : 'field';
+                        errorMsg.textContent = ` Please enter ${labelText}`
+                    }
+                } else {
+                    input.style.borderColor = ''
+                    input.style.backgroundColor = ''
+
+                    if(label) {
+                        label.style.color = ''
+                    }
+
+                    if(errorMsg) {
+                        errorMsg.remove()
+                    }
+                }
+            })
+
+            if(!isValid){
+                return;
+            }
+
+
+            try {
+                const response = await fetch(modalForm.action, {
+                    method: "POST",
+                    body: formData,
+                    headers: {
+                        'X-CSRFToken': formData.get('csrfmiddlewaretoken')
+                    }
+                });
+
+                if (response.ok) {
+                    window.location.href = response.url;
+                } else {
+                    alert('Error creating reservation');
+                }
+            } catch (error) {
+                const data = await response.json()
+                alert(data.error || 'Error creating reservation')
+            }
+        });
+    }
+});
+    
+    
--- primal_training/views.py
+++ primal_training/views.py
@@ -3,6 +3,9 @@ from .models import User, FitnessClass, Reservation
 from django.views.generic import TemplateView, ListView,DetailView
 from django.views.generic.edit import CreateView
 from django.views import View
+from django.utils.dateparse import parse_datetime
+from  django.db.models import Q
+from django.http import JsonResponse
 # Create your views here.
 
 class HomePage(TemplateView):
@@ -33,19 +36,41 @@ class ReservationCreateView(View):
         email = request.POST.get('email')
         phone = request.POST.get('phone')
         reser_date= request.POST.get('reser_date')
+        reser_date = parse_datetime(reser_date) if reser_date else None
 
-        user, _  = User.objects.get_or_create(
-            email=email,
-            defaults={
-                'name':name,
-                'last_name':last_name,
-                'phone':phone,
-            }
 
-        )
+
+        existing_user = User.objects.filter(Q(email=email) | Q(phone=phone)).first()
+
+        if existing_user:
+            
+            if existing_user.email == email and (existing_user.name != name or existing_user.last_name != last_name):
+                return JsonResponse({'error': 'A user with this email address already exists.'}, status=400)
+            
+            
+            if existing_user.phone == phone and (existing_user.name != name or existing_user.last_name != last_name):
+                return JsonResponse({'error': 'A user with this phone number already exists.'}, status=400)
+            
+
+            user = existing_user
+        else:
+            
+            user = User.objects.create(
+                name=name,
+                last_name=last_name,
+                email=email,
+                phone=phone
+            )
+
+        
+        if Reservation.objects.filter(user=user, fitness_class=fitness_class, reser_date=reser_date).exists():
+            return JsonResponse({'error': 'You have already created a reservation for this time.'}, status=400)
+
+        
         Reservation.objects.create(
             user=user,
-            fitness_class = fitness_class,
+            fitness_class=fitness_class,
             reser_date=reser_date
         )
+
         return redirect('HOME')
